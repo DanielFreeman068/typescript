ac75e62e6d40a105bea4600ac3e23163bfea5503
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2025-03-25 15:17:40 -0700

    start

---
+++ index.js
@@ -0,0 +1,5 @@
+var jersey_number = 17;
+var sigma = true;
+var player_name = 'Kevin De Bruyne';
+var greeting = "Yo yo yo";
+jersey_number = 9;
---
+++ index.ts
@@ -0,0 +1,8 @@
+var jersey_number : number = 17;
+var sigma : boolean = true;
+var player_name : string = 'Kevin De Bruyne';
+
+let greeting = "Yo yo yo"
+
+jersey_number = 9
+
