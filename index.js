bae1f564430aaae7757a938af17e38d872a2a4c4
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2025-04-08 15:19:00 -0700

    ypup

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+"use strict";
 var jersey_number = 17;
 var sigma = true;
 var player_name = 'Kevin De Bruyne';
-var greeting = "Yo yo yo";
+let greeting = "Yo yo yo";
 jersey_number = 9;
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(17, 89));
--- index.ts
+++ index.ts
@@ -6,3 +6,8 @@ let greeting = "Yo yo yo"
 
 jersey_number = 9
 
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+console.log(add(17, 89))
