ec7300ba8a32ed4e0a37bccdd8a83c7f96b06b9b
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2025-04-09 15:10:45 -0700

    update notes

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ var sigma = true;
 var player_name = 'Kevin De Bruyne';
 let greeting = "Yo yo yo";
 jersey_number = 9;
-function add(n1, n2) {
-    return n1 + n2;
-}
-console.log(add(17, 89));
+// function add(n1: number, n2: number) {
+//     return n1 + n2;
+// }
+// console.log(add(17, 89))
+const addArrow = (n1, n2) => n1 + n2;
+const printResult = (num) => {
+    console.log('result: ' + num);
+};
+let result = 18;
--- index.ts
+++ index.ts
@@ -6,8 +6,17 @@ let greeting = "Yo yo yo"
 
 jersey_number = 9
 
-function add(n1: number, n2: number) {
-    return n1 + n2;
+// function add(n1: number, n2: number) {
+//     return n1 + n2;
+// }
+
+// console.log(add(17, 89))
+
+const addArrow = (n1:number, n2:number) : number => n1 + n2
+
+const printResult = (num: number) : void => {
+    console.log('result: ' + num);
 }
 
-console.log(add(17, 89))
+let result : string | number = 18;
+
